6e58d8d011db7d88cf38a68f66eb6d98d7958a26
half-winter-moon <han16064@byui.edu>
2021-05-05 19:12:36 -0600

    site plan

---
+++ lesson3/test.js
@@ -0,0 +1,10 @@
+function main() {
+    var depth = parseInt(readLine(), 10);
+    //your code goes here
+    let day=1;
+    for (i=0; i>=depth;) {
+        let i=i+7-2;
+        let day += 1;
+        return day;
+    }
+}
