15730097d0ceab531604832639bb4c99ccaceb4d
half-winter-moon <han16064@byui.edu>
2021-05-14 10:28:07 -0600

    new lesson 5 folder

---
+++ lesson5/js/preston-5.js
@@ -0,0 +1,23 @@
+// putting this code within the evenlistener, listening for the page's load makes it possible
+// to see the id's and such right away/early, otherwise the program will think it doesn't exist yet
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    // To solve the mid resizing issue with responsice class on
+    // window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+});
+
+// Retrieve and display the current year
+let d = new Date();
+let year = d.getFullYear()
+document.getElementById("currentYear").textContent = year;
+
+// Display the current date
+
+let currentDate = new Date();
+//I couldn't figure out how to format the date nicely with the following line of code
+//formattedDate = currentDate.format('dd-m-yy');
+document.getElementById("currentDate").textContent = currentDate
