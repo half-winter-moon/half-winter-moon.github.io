bfd913b48eac5a92023bd9edbffd95122eda25ca
half-winter-moon <han16064@byui.edu>
2021-05-21 15:55:56 -0600

    updated student portal and preston town page continued

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -8,16 +8,33 @@ window.addEventListener('load', () => {
 
     // To solve the mid resizing issue with responsice class on
     // window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-});
 
+    // Display the current date
+    let currentDate = new Date();
+    // let weekDay = new Array(7);
+    // weekDay[0] = 'Sunday';
+    // weekDay[1] = 'Monday';
+    // weekDay[2] = 'Tuesday';
+    // weekDay[3] = 'Wednesday';
+    // weekDay[4] = 'Thursday';
+    // weekDay[5] = 'Friday';
+    // weekDay[6] = 'Saturday';
+    // week.textContent = weekDay[currentDate.getDay()];
+
+    // If it is friday, display an announcement about pancakes in the park
+    const pancakeDay = document.querySelector('#pancakeDay');
+    if (currentDate.getDay() == 5) {
+        pancakeDay.style.display = 'block';
+    }
+    else {
+        pancakeDay.style.display = 'none';
+    }
+
+    document.getElementById("currentDate").textContent = currentDate;
 // Retrieve and display the current year
-let d = new Date();
-let year = d.getFullYear()
-document.getElementById("currentYear").textContent = year;
+    let d = new Date();
+    let year = d.getFullYear()
+    document.getElementById("currentYear").textContent = year;
 
-// Display the current date
 
-let currentDate = new Date();
-//I couldn't figure out how to format the date nicely with the following line of code
-//formattedDate = currentDate.format('dd-m-yy');
-document.getElementById("currentDate").textContent = currentDate
+});
