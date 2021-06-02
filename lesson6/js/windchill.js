83949a9581798a68918889edc3ee688e6435a6b9
half-winter-moon <han16064@byui.edu>
2021-06-02 10:43:03 -0600

    updated wind chill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,10 +1,13 @@
-var highTemp = parseInt(document.querySelector('#highTemp'));
-var windSpeed = parseInt(document.querySelector('#windSpeed'));
+var highTemp = parseInt(document.querySelector('#highTemp').textContent);
+var windSpeed = parseInt(document.querySelector('#windSpeed').textContent);
 var windChill = document.querySelector('#windChill');
 var windChillCalc = 0;
 
+
+
  if (highTemp <= 50 && windSpeed > 3) {
     windChillCalc = 35.74 + (0.6215 * highTemp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * highTemp * (windSpeed ** 0.16));
+    windChillCalc = Number((windChillCalc).toFixed(1)); 
     console.log(windChillCalc);
     windChill.innerHTML = windChillCalc;
  } else {
