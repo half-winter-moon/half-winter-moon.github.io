cffbdba05c38c0f7b9f5f521980667c990cc64a6
half-winter-moon <han16064@byui.edu>
2021-07-01 15:26:14 -0600

    updates

--- lesson11/js/index.js
+++ lesson11/js/index.js
@@ -5,107 +5,76 @@ window.addEventListener('load', () => {
     const mainnav = document.querySelector('#navigation');
 
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-    // To solve the mid resizing issue with responsice class on
-    // window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-    // Display the current date
-    let currentDate = new Date();
-
-    // If it is friday, display an announcement about pancakes in the park
-    const pancakeDay = document.querySelector('#pancakeDay');
-    if (currentDate.getDay() == 5) {
-        pancakeDay.style.display = 'block';
-    }
-    else {
-        pancakeDay.style.display = 'none';
-    }
-    document.getElementById("currentDate").textContent = currentDate;
-// Retrieve and display the current year
-    let d = new Date();
-    let year = d.getFullYear()
-    document.getElementById("currentYear").textContent = year;
-
-
-    //grab next five days forcast from API and display
-    const requestURL = 'https://api.openweathermap.org/data/2.5/forecast?zip=83263&appid=699c1da2022aa5124ee05e7d3550a1d1&units=imperial';
+    
+    const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
     fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (preston_data) {
-        
-        console.table(preston_data);        
-        let temp1 = document.querySelector('#temp1');
-        let temp2 = document.querySelector('#temp2');
-        let temp3 = document.querySelector('#temp3');
-        let temp4 = document.querySelector('#temp4');
-        let temp5 = document.querySelector('#temp5');
-
-        temp1.textContent = Math.round(preston_data.list[0].main.temp);
-        temp2.textContent = Math.round(preston_data.list[1].main.temp);
-        temp3.textContent = Math.round(preston_data.list[2].main.temp);
-        temp4.textContent = Math.round(preston_data.list[3].main.temp);
-        temp5.textContent = Math.round(preston_data.list[4].main.temp);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (jsonObject) {
+            //console.table(jsonObject); //temporary checking for valid respons and data parsing
+            const towns = jsonObject['towns'];
+            
+            //for (let i = 0; i < towns.length; i++) {
     
-        // Grab appropriate weather icons from API for 5 day forecast
-        let icon1 = document.querySelector('#icon1');
-        let imgInfo1 = preston_data.list[0].weather[0].icon;
-        icon1.setAttribute('src', 'http://openweathermap.org/img/wn/' + imgInfo1 + '@2x.png');
+            //Preston info:
+            let tagLine = document.querySelector('.tagline');
+            tagLine.textContent = towns[6].motto;
 
-        let icon2 = document.querySelector('#icon2');
-        let imgInfo2 = preston_data.list[1].weather[0].icon;
-        icon2.setAttribute('src', 'http://openweathermap.org/img/wn/' + imgInfo2 + '@2x.png');
+            let info = document.createElement('section');
+            let p1 = document.createElement('p');
+            p1.textContent = 'Year Founded: ' + towns[6].yearFounded;
+            info.appendChild(p1);
 
-        let icon3 = document.querySelector('#icon3');
-        let imgInfo3 = preston_data.list[2].weather[0].icon;
-        icon3.setAttribute('src', 'http://openweathermap.org/img/wn/' + imgInfo3 + '@2x.png');
+            let p2 = document.createElement('p');
+            p2.textContent = 'Population: ' + towns[6].currentPopulation;
+            info.appendChild(p2);
 
-        let icon4 = document.querySelector('#icon4');
-        let imgInfo4 = preston_data.list[3].weather[0].icon;
-        icon4.setAttribute('src', 'http://openweathermap.org/img/wn/' + imgInfo4 + '@2x.png');
+            let p3 = document.createElement('p');
+            p3.textContent = 'Annual Rain Fall: ' + towns[6].averageRainfall;
+            info.appendChild(p3);
 
-        let icon5 = document.querySelector('#icon5');
-        let imgInfo5 = preston_data.list[4].weather[0].icon;
-        icon5.setAttribute('src', 'http://openweathermap.org/img/wn/' + imgInfo5 + '@2x.png');
-})
+            document.querySelector('div.preston').appendChild(info);
 
-    // grab current day weather info from API and display
-    const requestURL2 = 'https://api.openweathermap.org/data/2.5/weather?zip=83263,US&units=imperial&appid=699c1da2022aa5124ee05e7d3550a1d1';
+            //Soda Springs info:
+            let tagLine2 = document.querySelector('.tagline');
+            tagLine2.textContent = towns[0].motto;
 
-    fetch(requestURL2)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (present_data) {
-        console.table(present_data);
+            let info2 = document.createElement('section');
+            let sp1 = document.createElement('p');
+            sp1.textContent = 'Year Founded: ' + towns[0].yearFounded;
+            info2.appendChild(sp1);
 
-        let currently = document.querySelector('#currently');
-        currently.textContent = present_data.weather[0].main;
+            let sp2 = document.createElement('p');
+            sp2.textContent = 'Population: ' + towns[0].currentPopulation;
+            info2.appendChild(sp2);
 
-        let windSpeed = document.querySelector('#windSpeed');
-        windSpeed.textContent = Math.round(present_data.wind.speed);
+            let sp3 = document.createElement('p');
+            sp3.textContent = 'Annual Rain Fall: ' + towns[0].averageRainfall;
+            info2.appendChild(sp3);
 
-        let highTemp = document.querySelector('#highTemp');
-        highTemp.textContent = Math.round(present_data.main.temp_max);
+            document.querySelector('div.soda-springs').appendChild(info2);
 
-        let humidity = document.querySelector('#humidity');
-        humidity.textContent = present_data.main.humidity;
+            //Fish Haven info:
+            let tagLine3 = document.querySelector('.tagline');
+            tagLine3.textContent = towns[2].motto;
 
+            let info3 = document.createElement('section');
+            let fp1 = document.createElement('p');
+            fp1.textContent = 'Year Founded: ' + towns[2].yearFounded;
+            info3.appendChild(fp1);
 
-    // calculate and display windchill
-    var highTemp2 = parseInt(document.querySelector('#highTemp').textContent);
-    var windSpeed2 = parseInt(document.querySelector('#windSpeed').textContent);
-    var windChill2 = document.querySelector('#windChill');
-    var windChillCalc = 0;
-    if (highTemp <= 50 && windSpeed2 > 3) {
-        windChillCalc = 35.74 + (0.6215 * highTemp2) - (35.75 * (windSpeed2 ** 0.16)) + (0.4275 * highTemp2 * (windSpeed2 ** 0.16));
-        windChillCalc = Number((windChillCalc).toFixed(1)); 
-        console.log(windChillCalc);
-        windChill.innerHTML = windChillCalc;
-    } else {
-        windChill.innerHTML = 'N/A';
-    }
-    })
+            let fp2 = document.createElement('p');
+            fp2.textContent = 'Population: ' + towns[2].currentPopulation;
+            info3.appendChild(fp2);
+
+            let fp3 = document.createElement('p');
+            fp3.textContent = 'Annual Rain Fall: ' + towns[2].averageRainfall;
+            info3.appendChild(fp3);
+
+            document.querySelector('div.fish-haven').appendChild(info3);
+        })
+ 
+
 });
