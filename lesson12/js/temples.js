bc7b30c69485e6000c7b3fdbd3c8139473fbceaa
half-winter-moon <han16064@byui.edu>
2021-07-14 15:42:58 -0600

    dropdown menu

--- lesson12/js/temples.js
+++ lesson12/js/temples.js
@@ -1,9 +1,13 @@
 window.addEventListener('load', () => {
-   
     const hambutton = document.querySelector('.ham');
     const mainnav = document.querySelector('#navigation');
-    
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    // Retrieve and display the current year
+    let d = new Date();
+    let year = d.getFullYear()
+    document.getElementById("currentYear").textContent = year;
+
 
     const requestURL = "js/temples_json.json";
 })
