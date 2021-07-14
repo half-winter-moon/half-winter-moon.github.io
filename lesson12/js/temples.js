49066a71a8dd8d9267a3859d66c70490b6f3e8f2
half-winter-moon <han16064@byui.edu>
2021-07-14 10:53:24 -0600

    beginning home page

--- lesson12/js/temples.js
+++ lesson12/js/temples.js
@@ -1,3 +1,9 @@
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
+   
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+    
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
     const requestURL = "js/temples_json.json";
 })
